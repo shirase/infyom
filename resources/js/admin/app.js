3394733711be2e94ab4e9061c41429e9fbf177a7
Andrey Akimov <shirase55@gmail.com>
2021-02-22 15:07:20 +0300

    -

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -8,25 +8,27 @@ require('icheck');
 require('select2');
 require('jstree');
 
-document.querySelectorAll('textarea[data-ckeditor]').forEach(function(element) {
-    CKEDITOR.replace(element, {
-        extraAllowedContent: 'iframe[*];script;style;blockquote;img[*]{*}(*);div[*]{*}(*);span[*]{*}(*);p[*]{*}(*);',
-        extraPlugins: 'sourcedialog,image-uf',
-        removePlugins: 'image',
-        toolbar: [
-            ['Sourcedialog'],
-            ['Undo', 'Redo'],
-            ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteCode'],
-            ['Bold', 'Italic'],
-            ['NumberedList', 'BulletedList', 'Outdent', 'Indent'],
-            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-            ['Link', 'Unlink'],
-            ['FontSize', 'Styles', 'Format'],
-            ['Table', 'image-uf', 'Widgets'],
-            ['RemoveFormat'],
-        ],
-        filebrowserBrowseUrl: '/vendor/kcfinder/browse.php?opener=ckeditor&type=files',
-        filebrowserUploadUrl: '/vendor/kcfinder/upload.php?opener=ckeditor&type=files',
-        filebrowserUploadMethod: 'form',
-    });
+document.addEventListener('click', function(e) {
+    if (e.target.hasAttribute('data-ckeditor')) {
+        CKEDITOR.replace(e.target, {
+            extraAllowedContent: 'iframe[*];script;style;blockquote;img[*]{*}(*);div[*]{*}(*);span[*]{*}(*);p[*]{*}(*);',
+            extraPlugins: 'sourcedialog,image-uf',
+            removePlugins: 'image',
+            toolbar: [
+                ['Sourcedialog'],
+                ['Undo', 'Redo'],
+                ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteCode'],
+                ['Bold', 'Italic'],
+                ['NumberedList', 'BulletedList', 'Outdent', 'Indent'],
+                ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+                ['Link', 'Unlink'],
+                ['FontSize', 'Styles', 'Format'],
+                ['Table', 'image-uf', 'Widgets'],
+                ['RemoveFormat'],
+            ],
+            filebrowserBrowseUrl: '/vendor/kcfinder/browse.php?opener=ckeditor&type=files',
+            filebrowserUploadUrl: '/vendor/kcfinder/upload.php?opener=ckeditor&type=files',
+            filebrowserUploadMethod: 'form',
+        });
+    }
 })
