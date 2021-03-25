9beb0621cd01b5dd21363e7aad62bfd3d5205b9c
Andrey Akimov <shirase55@gmail.com>
2021-03-26 01:19:42 +0300

    -

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -14,6 +14,11 @@
 
         @foreach($models as $model)
             <div class="my-4">
+                @if($model->image_path)
+                    <div class="mb-2">
+                        <img src="{{ \App\Helpers\GlideHelper::createPublicUrl($model->image_path, ['w' => 150]) }}">
+                    </div>
+                @endif
                 <p class="lead">{{ $model->title }}</p>
                 @if ($model->description)<p>{{ $model->description }}</p>@endif
                 <p><a href="{{ $category ? route('article.category.show', ['slug' => $model->slug, 'category' => $category->slug]) : route('article.show', ['slug' => $model->slug]) }}">Подробнее</a></p>
