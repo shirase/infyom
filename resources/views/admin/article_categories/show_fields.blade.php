1b8919b7d9f1ebe5b3f3e3dacf56028e27b20c97
Andrey Akimov <shirase55@gmail.com>
2021-03-25 22:18:27 +0300

    -

--- resources/views/admin/article_categories/show_fields.blade.php
+++ resources/views/admin/article_categories/show_fields.blade.php
@@ -9,3 +9,9 @@
     {!! Form::label('slug', __('fields.slug')) !!}
     <p>{{ $articleCategory->slug }}</p>
 </div>
+
+<!-- Status Field -->
+<div class="form-group">
+    {!! Form::label('status', __('fields.status')) !!}
+    <p>{{ $articleCategory->statusName() }}</p>
+</div>
