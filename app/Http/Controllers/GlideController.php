0d1cafba78b89aae0857c20f9d463435657a10a1
Andrey Akimov <shirase55@gmail.com>
2021-03-26 00:07:51 +0300

    -

--- app/Http/Controllers/GlideController.php
+++ app/Http/Controllers/GlideController.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Filesystem\FilesystemManager;
 use League\Glide\Responses\LaravelResponseFactory;
 use League\Glide\ServerFactory;
 
 class GlideController extends Controller
 {
-    public function show(Filesystem $filesystem, $path)
+    public function show(Container $app, $path)
     {
+        /** @var FilesystemManager $filesystem */
+        $filesystem = $app['filesystem'];
+        /** @var FilesystemAdapter $disk */
+        $disk = $filesystem->disk('local');
+
         $server = ServerFactory::create([
             'response' => new LaravelResponseFactory(app('request')),
-            'source' => $filesystem->getDriver(),
-            'cache' => $filesystem->getDriver(),
+            'source' => $disk->getDriver(),
+            'cache' => $disk->getDriver(),
             'cache_path_prefix' => '.cache',
             'base_url' => 'glide',
         ]);
