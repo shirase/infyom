b9b65c4051648196c652517eab1ebf7b394ea8d5
Andrey Akimov <shirase55@gmail.com>
2023-01-31 09:54:25 +0300

    Frame dialog reload on changes

--- resources/js/frame_dialog.js
+++ resources/js/frame_dialog.js
@@ -2,6 +2,8 @@ $(document).on('click', '.j-frame-dialog, .j_frame_dialog', function(event) {
     event.preventDefault();
     const el = $(this);
 
+    let iFrameHasChanges = null;
+
     bodyFixed();
 
     const iframePopup = $('<div class="iframe_popup" />').appendTo('body');
@@ -11,6 +13,10 @@ $(document).on('click', '.j-frame-dialog, .j_frame_dialog', function(event) {
         iframePopup.remove();
         $(document).off('action');
         bodyUnfixed();
+
+        if (iFrameHasChanges) {
+            location.reload();
+        }
     });
 
     const expand = $('<a class="iframe_popup__expand" />').appendTo(iframePopupIframe);
@@ -23,34 +29,15 @@ $(document).on('click', '.j-frame-dialog, .j_frame_dialog', function(event) {
     iframe.on('iframeloading', function() {
         iframe.contents().find('body').addClass('is-frame-dialog');
         iframe.contents().find('body').addClass('sidebar-collapse');
+
+        // second load
+        if (iFrameHasChanges === false) {
+            iFrameHasChanges = true;
+        }
+        if (iFrameHasChanges === null) {
+            iFrameHasChanges = false;
+        }
     });
 
     iframe.attr('src', el.attr('href'));
-
-    const dataType = el.data('type');
-    if(dataType=='index') {
-        close.click(function() {
-            location.reload();
-        });
-    }
-    if(dataType=='create') {
-        $(document).on('action.Create', function() {
-            location.reload();
-        });
-    }
-    if(dataType=='update') {
-        $(document).on('action.Delete', function() {
-            location.reload();
-        });
-        $(document).on('action.Update', function() {
-            location.reload();
-        });
-    }
 });
-
-function receiveMessage(event) {
-    if (event.data.action) {
-        $(document).trigger('action.' + event.data.action);
-    }
-}
-window.addEventListener("message", receiveMessage, false);
